aa098fed2627287eb6564a2d56d882450cde9fa1
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-01-18 12:52:26 +0000

    Update

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var express = require('express')
 var ejs     = require('ejs')
 var body    = require('body-parser')
 var cookie  = require('cookie-parser')
+var mysql   = require('mysql')
+var database = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'p@ssword',
+  database: 'dekidol'
+}
+var pool    = mysql.createPool(database)
 var app     = express()
 var valid   = [ ]
 app.engine('html', ejs.renderFile)
-app.listen(2000)
+app.listen(80)
 app.use( body.urlencoded({extended:false}) )
 app.use( cookie() )
 app.get('/', showIndex)
@@ -14,6 +22,9 @@ app.get('/login', showLogInPage)
 app.post('/login', checkLogIn)
 app.get('/profile', showProfilePage)
 app.get('/logout', logoutUser)
+app.get('/register', showRegisterPage)
+app.post('/register', saveNewUser)
+
 app.use( express.static('public') )
 app.use( showError )
 function showIndex(req, res)     { res.render('index.html') }
@@ -53,3 +64,28 @@ function logoutUser(req, res) {
     res.redirect('/')
 }
 
+function showRegisterPage(req, res) {
+	res.render('register.html')
+}
+
+function saveNewUser(req, res) {
+	pool.query('insert into member(name, password, full_name) ' +
+		'values(?,sha2(?, 512),?)',
+		[req.body.name, req.body.password, req.body.full_name],
+		(error, data) => {
+			res.redirect('/login')
+		}
+	)
+}
+
+
+
+
+
+
+
+
+
+
+
+//
