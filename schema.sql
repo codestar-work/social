b1376df90f9e8af63b90c1dff1dce371057f3709
Kookiat Suetrong <beer@Kookiats-MacBook-Air.local>
2017-01-18 20:49:43 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,10 @@
--- https://github.com/codestar-work/social
-
 create database dekidol default charset='utf8';
 create user dekidol@'%' identified by 'dekidol7';
 grant all on dekidol.* to dekidol@'%';
 use dekidol;
 create table member (
   id   serial,
-  name varchar(255),
+  name varchar(255) unique not null,
   password varchar(1023),
   full_name varchar(255),
   info  varchar(1023),
@@ -14,5 +12,3 @@ create table member (
 );
 insert into member(name, password, full_name)
 values('markz', sha2('mark123', 512), 'Mark Zuckerberg');
-
-
